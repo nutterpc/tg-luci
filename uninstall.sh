3d719c6546f090eadefad0781ecca3ce620061ed
Francesco Molitierno <franc.molitierno@gmail.com>
2018-10-27 15:14:42 +0200

    fix segfault recursion

--- uninstall.sh
+++ uninstall.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
-logger() {
-	logger -s -t "tg-LuCI Install Script:" "$1"
-	echo "$1"
+log() {
+	logger -s -t "tg-LuCI Install Script" "$1"
 }
 
-logger "Backing up uci.so..."
+log "Backing up uci.so..."
 mv /usr/lib/lua/uci.so /usr/lib/lua/uci.so_bak
 
-logger "Removing LuCI and dependancies..."
+log "Removing LuCI and dependancies..."
 opkg remove --force-depends luci luci-lib-json luci-lib-jsonc luci-lib-nixio luci-mod-rpc libiwinfo-lua libiwinfo libuci-lua rpcd-mod-file rpcd-mod-iwinfo rpcd-mod-rpcsys rpcd uhttpd-mod-lua uhttpd-mod-ubus uhttpd
 
-logger "Restoring tch uci.so..."
+log "Restoring tch uci.so..."
 rm /usr/lib/lua/uci.so
 mv /usr/lib/lua/uci.so_bak /usr/lib/lua/uci.so
 
-logger "Removing www_luci and config Files..."
+log "Removing www_luci and config Files..."
 rm -R /www_luci
 rm /etc/config/rpcd
 rm /etc/config/uhttpd
 
-logger "Done, LuCI removed!"
+log "Done, LuCI removed!"
